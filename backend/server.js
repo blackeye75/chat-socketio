9ec72ad33fc78372db74a53e71929624f6af9e4a
blackeye75 <priyanshuraj22275@gmail.com>
2025-10-07 16:57:12 +0530

    Add chat-socketio package and update dependencies in frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,26 @@ import dotenv from 'dotenv';
 dotenv.config();
 import authRouter from './routes/auth.routes.js';
 import messageRouter from './routes/message.route.js';
+import path from 'path';
 
 
 const app = express();
+const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
 
 
 app.use('/api/auth', authRouter)
-app.use('/api/message', authRouter)
+app.use('/api/message', messageRouter)
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/.*/, (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(3000, () => { console.log('Server is running on port 3000'); });
