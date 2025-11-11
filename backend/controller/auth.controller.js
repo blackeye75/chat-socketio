94a7e52813ad557be07226eac1e2b4dcf455e119
blackeye75 <priyanshuraj22275@gmail.com>
2025-11-11 17:32:43 +0530

    Refactor authentication controller to improve signup and implement login functionality

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -32,7 +32,7 @@ export const signup = async (req, res) => {
     })
 
     if (newUser) {
-      const savedUser =await newUser.save();
+      const savedUser = await newUser.save();
       generateToken(savedUser._id, res);
       res.status(201).json({
         _id: newUser._id,
@@ -52,4 +52,33 @@ export const signup = async (req, res) => {
     console.error(`Error in signup: ${error.message}`);
     res.status(500).json({ message: "Server error" });
   }
+}
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "Invalid email or password" });
+    
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) return res.status(400).json({ message: "Invalid email or password" });
+    generateToken(user._id, res);
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic
+    });
+  } catch (error) {
+    console.error(`Error in login: ${error.message}`);
+    res.status(500).json({ message: "Server error" });
+  }
+}
+
+export const logout = (req, res) => {
+  res.cookie('token', '', {
+    maxAge: 0,
+    httpOnly: true,
+  })
+  res.status(200).json({ message: "Logged out successfully" });
 }
--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signup } from '../controller/auth.controller.js';
+import { login, logout, signup } from '../controller/auth.controller.js';
 
 const authRouter = express.Router();
 
-authRouter.post('/signup', signup )
-// authRouter.post('/signin', signin )
+authRouter.post('/signup', signup)
+authRouter.post('/login', login);
+authRouter.post('/logout', logout);
 
 export default authRouter
