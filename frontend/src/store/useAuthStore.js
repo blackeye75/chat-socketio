b028f40ca77438fce03cd99bb7f5984dc0799319
blackeye75 <priyanshuraj22275@gmail.com>
2025-11-19 20:44:58 +0530

    feat: enhance ProfileHeader with image upload and sound toggle functionality

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -46,7 +46,7 @@ export const useAuthStore = create((set) => ({
       set({ isLoggingIn: false });
     }
   },
-   logout: async () => {
+  logout: async () => {
     try {
       await axiosInstance.post("/auth/logout");
       set({ authUser: null });
@@ -57,5 +57,14 @@ export const useAuthStore = create((set) => ({
       console.log("Logout error:", error);
     }
   },
-
+  updateProfile: async (data) => {
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("Error in update profile:", error);
+      toast.error(error.response.data.message);
+    }
+  },
 }))
