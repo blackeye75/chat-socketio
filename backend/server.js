610e5446bd10e2d663181b7d6bab42b557279d8b
blackeye75 <priyanshuraj22275@gmail.com>
2025-12-02 14:37:52 +0530

    #

--- backend/server.js
+++ backend/server.js
@@ -22,13 +22,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 // make ready for deployment
-// if (ENV.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//   app.get("*", (_, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-//   });
-// }
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server running on port: " + PORT);
